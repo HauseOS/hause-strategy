4a1553843bde441bcae29e645dac0dc2d7ee0e67
Aurelius <os@hause.co>
2026-02-27 21:54:28 +0800

    Fix: Remove unused attribute parameter

--- lib/theme-provider.tsx
+++ lib/theme-provider.tsx
@@ -4,14 +4,12 @@ import * as React from "react";
 
 interface ThemeProviderProps {
   children: React.ReactNode;
-  attribute?: string;
   defaultTheme?: string;
   enableSystem?: boolean;
 }
 
 export function ThemeProvider({
   children,
-  attribute = "class",
   defaultTheme = "dark",
   enableSystem = true,
 }: ThemeProviderProps) {
